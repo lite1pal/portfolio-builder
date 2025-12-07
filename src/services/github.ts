5084cef51803a276d8cddf2ec613801cc22d2901
Denis <deniskatarasenko6@gmail.com>
2025-12-07 14:37:20 +0100

    feat: refactor fetchPublicRepos function to include type definition for improved type safety

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,9 +1,7 @@
-export async function fetchPublicRepos(username: string) {
+import type { Repo } from "../types/Repo";
+
+export async function fetchPublicRepos(username: string): Promise<Repo[]> {
   const res = await fetch(`https://api.github.com/users/${username}/repos`);
-
   if (!res.ok) throw new Error(`Github API error: ${res.status}`);
-
-  const repos = await res.json();
-  console.log(repos);
-  return repos;
+  return res.json();
 }
