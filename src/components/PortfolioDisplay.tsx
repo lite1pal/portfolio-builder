de99d4b1b01d3e15fabb3882286c3adf3446aa4d
Denis <deniskatarasenko6@gmail.com>
2025-12-07 12:59:17 +0100

    feat: enhance PortfolioDisplay layout with improved styling and structure

--- src/components/PortfolioDisplay.tsx
+++ src/components/PortfolioDisplay.tsx
@@ -8,12 +8,9 @@ export default function PortfolioDisplay({ portfolio }: PortfolioDisplayProps) {
   return (
     <div className="mockup-phone max-w-sm border-[#ff8938]">
       <div className="mockup-phone-camera"></div>
-      <div className="mockup-phone-display p-5">
+      <div className="mockup-phone-display p-5 flex flex-col gap-3">
         <div className="text-xl font-semibold">{portfolio.name}</div>
-        {/* <img
-          alt="wallpaper"
-          src="https://img.daisyui.com/images/stock/453966.webp"
-        /> */}
+        <div className="font-semibold">{portfolio.description}</div>
       </div>
     </div>
   );
