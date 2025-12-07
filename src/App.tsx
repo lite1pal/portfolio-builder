a85894af68d0bd9f514b15eefb5fabd16a0c876e
Denis <deniskatarasenko6@gmail.com>
2025-12-07 10:44:00 +0100

    feat: wrap ProfileForm component in a div for improved layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import ProfileForm from "./components/ProfileForm";
 
 function App() {
   return (
-    <>
+    <div className="flex px-4 sm:px-8 items-center min-h-screen justify-center">
       <ProfileForm />
-    </>
+    </div>
   );
 }
 
